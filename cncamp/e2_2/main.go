b43ecd331b2c304200b3f8b8ee3c22be2b20f914
Marvin Deng <marvin.deng@hotmail.com>
2022-04-22 11:47:57 +0800

    HeadleFunc

--- cncamp/e2_2/main.go
+++ cncamp/e2_2/main.go
@@ -5,22 +5,20 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
-	http.HandleFunc("/healthz", healthz)
-	http.ListenAndServe(":80", MyServer{})
+	http.HandleFunc("/", copyHeaders)
+	http.HandleFunc("/healthz/", healthz)
+	http.ListenAndServe(":80", nil)
 }
 
 func healthz(w http.ResponseWriter, r *http.Request) {
+	copyHeaders(w, r)
 	io.WriteString(w, "200")
 }
 
-type MyServer struct {
-}
-
-func (server MyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func copyHeaders(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Header {
 		for _, m := range v {
 			w.Header().Add(k, m)
@@ -31,5 +29,5 @@ func (server MyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	version := os.Getenv(VERSION)
 	w.Header().Set(VERSION, version)
 
-	fmt.Printf("IP:%s, Status:%d", strings.Split(r.RemoteAddr, ":")[0], http.StatusOK)
+	fmt.Printf("RemoteAddress:%s, Status:%d", r.RemoteAddr, http.StatusOK)
 }
